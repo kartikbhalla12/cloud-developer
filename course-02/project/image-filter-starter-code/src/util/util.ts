cfd297fccc4ce7fa6b9b14eaaa4adbb483dc0877
kartik <kartikbhalla12@gmail.com>
2021-02-25 17:06:09 +0530

    util: add exception handler for filterImageFromURL
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -8,18 +8,24 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
-    });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const photo = await Jimp.read(inputURL);
+			const outPath =
+				'/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+			await photo
+				// .resize(256, 256) // resize
+				.quality(100) // set JPEG quality
+				.scaleToFit(512, 512) //maintain aspect ratio
+				// .grayscale() // do not set grayscale
+				.write(__dirname + outPath, img => {
+					resolve(__dirname + outPath);
+				});
+		} catch (ex) {
+			reject('Invalid URL or Can not fetch image!');
+		}
+	});
 }
 
 // deleteLocalFiles
@@ -27,8 +33,8 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+export async function deleteLocalFiles(files: Array<string>) {
+	for (let file of files) {
+		fs.unlinkSync(file);
+	}
 }
